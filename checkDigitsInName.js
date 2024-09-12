ef1b12f33133bd58c44beb2fafc38f3d0e8b2c97
Md Rakibul Islam Rakib <mri.rakib03@gmail.com>
2024-09-12 20:33:03 +0600

    checkDigitsInName

--- checkDigitsInName.js
+++ checkDigitsInName.js
@@ -1,9 +1,17 @@
 function checkDigitsInName(name) {
     if (typeof name === 'string'){
-        return 2;
+        let nameArray = name.split('');
+        for (let i = 0; i < nameArray.length; i++) { 
+            for (let j = 0; j < 10; j++) {
+                if ( j == nameArray[i]) {
+                    return true;
+                } 
+            }   
+        }
+        return false;
     } else {
         return "Invalid Input";
     }
 }
 
-console.log(checkDigitsInName('3423'));
+console.log(checkDigitsInName('1Rds12'));
